7e9a5db4112967f0b50528823ec334a1f490ab0d
Phucdanghoc <lep61748@gmail.com>
2023-03-25 20:53:35 +0700

    created project springboot

---
+++ Midterm/src/test/java/tdtu/javatech/midterm/MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package tdtu.javatech.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
